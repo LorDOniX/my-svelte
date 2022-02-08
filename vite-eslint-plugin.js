3e67960a48feee961b5a37958fc90af4c2773a1c
Roman Makudera <roman.makudera@firma.seznam.cz>
2022-02-08 15:12:49 +0100

    spravne nastaveni pro hmr

--- vite-eslint-plugin.js
+++ vite-eslint-plugin.js
@@ -1,4 +1,7 @@
 const { ESLint } = require("eslint");
+const SRC_PATH = "src";
+const TESTS_PATH = "__tests__";
+const EXTENSIONS = [".svelte", ".ts"];
 
 export default () => {
 	const eslint = new ESLint();
@@ -9,11 +12,19 @@ export default () => {
 		return formatter.format(results);
 	}
 
+	function testExtension(path, extensions) {
+		for (let ind = 0, max = extensions.length; ind < max; ind++) {
+			if (path.indexOf(extensions[ind]) != -1) return true;
+		}
+
+		return false;
+	}
+
 	return {
 		name: 'eslintPlugin',
 
 		async buildStart() {
-			const resultText = await lintFiles(["./src"]);
+			const resultText = await lintFiles([`./${SRC_PATH}`]);
 
 			setTimeout(() => {
 				console.log(resultText || "Main build no-errors");
@@ -22,8 +33,9 @@ export default () => {
 
 		async handleHotUpdate(ctx) {
 			const fileName = ctx.file;
+			const srcPath = `${__dirname}/${SRC_PATH}/`;
 
-			if ([".svelte", ".ts"].indexOf(fileName) == -1 && fileName.indexOf("__tests__") != -1) return;
+			if (fileName.indexOf(srcPath) == -1 || fileName.indexOf(TESTS_PATH) != -1 || !testExtension(fileName, EXTENSIONS)) return;
 
 			const resultText = await lintFiles([fileName]);
 
